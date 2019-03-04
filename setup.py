28a6d0437c8a8ca8496624ad4c76e20d19fc50fc
Kunal Gupta <upanshug53@gmail.com>
2019-03-04 18:19:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pycryptokg',         # How you named your package folder (MyLib)
   packages = ['pycryptokg'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python library for some cryptographic algorithms',   # Give a short description about your library
   author = 'Kunal Gupta',                   # Type in your name
